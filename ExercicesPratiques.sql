d97f82507896a35dd58ed9b3cac7c65c0e07d052
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2025-12-03 23:43:00 +0100

    ajouter des fonctions

--- ExercicesPratiques.sql
+++ ExercicesPratiques.sql
@@ -1,16 +1,19 @@
 -- 1. INSERT - Création d'un nouvel utilisateur
 
-INSERT INTO users (username, email, role, pass_word) VALUES ('jdupont', 'jean.dupont@email.com', 'éditeur', '$2y$10$', 'admin', '$2y$10$...');
+INSERT INTO users (username, email, role, pass_word) 
+VALUES ('jdupont', 'jean.dupont@email.com', 'éditeur', '$2y$10$', 'admin', '$2y$10$...');
 
 
 --2. SELECT - Récupération d'articles
 
-SELECT  title,date_cr,statu from postes;
+SELECT  title,date_cr,statu
+ from postes;
 
 
 --3. UPDATE - Modification de statut
 
-UPDATE postes set statu="archivé" 
+UPDATE postes
+ set statu="archivé" 
 where date_cr<2024 and statu="brouillon";
 
 
@@ -28,11 +31,15 @@ where statu="spam"
 
 -- 5. WHERE - Filtrage temporel
 
-select * from  postes where   date_insc > "2024-12-01"
+select *
+ from  postes 
+ where   date_insc > "2024-12-01"
 
 -- 6. ORDER BY - Tri chronologique
 
-SELECT * FROM users ORDER BY date_insc ASC;
+SELECT *
+ FROM users
+  ORDER BY date_insc ASC;
 
 
 -- 7. LIMIT - Articles récents
@@ -45,3 +52,50 @@ select title, date_cr, date_md from postes
  --8. DISTINCT - Rôles uniques
 
  select DISTINCT role from users ; 
+
+
+
+
+
+-- Niveau 3 : Opérateurs Logiques
+
+--9. AND/OR - Articles par catégorie et statut
+
+SELECT * 
+FROM postes
+WHERE  id_cat=1 and (statu="published" or statu="draft");
+
+
+
+-- 10. BETWEEN - Commentaires par période
+
+select * 
+from comments
+where date_cr BETWEEN "2024-12-01" and  "2024-12-15";
+
+
+ --11. IN - Articles par catégories multiples
+
+SELECT p.*
+FROM postes p
+JOIN category c ON p.id_cat = c.id_cat
+WHERE c.nom_cat IN ('PHP', 'JavaScript', 'Base de données');
+
+
+-- 12. LIKE - Recherche par email
+
+select *
+from users
+where email like  "@gmail.com"
+
+
+
+--Niveau 4 : Fonctions d'Agrégation
+
+--Compter le nombre total d'articles publiés
+
+
+select count(*)
+from postes 
+where statu="published"
+
